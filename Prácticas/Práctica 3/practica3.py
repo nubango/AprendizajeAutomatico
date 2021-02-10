d0d91c727a9b2ab25598ce02814c5d27b6d0b2d0
Álvar de Diego López <alvarded@ucm.es>
2021-02-10 19:20:20 +0100

    P3 arreglada

--- Prácticas/Práctica 3/practica3.py
+++ Prácticas/Práctica 3/practica3.py
@@ -19,40 +19,40 @@ def sigmoid(Z):
     return 1/(1+np.exp(-Z))
 
 # Devuelve un valor de coste
-def cost(O, X, Y, l):
-    return (-(((np.log(sigmoid(X.dot(O)))).T.dot(Y) 
-+ (np.log(1 - sigmoid(X.dot(O)))).T.dot(1 - Y)) / X.shape[0])
-    + (1 / (2 * X.shape[0])) * (O[1:,]**2).sum())
+def cost(Theta, X, Y, l):
+    return (-(((np.log(sigmoid(X.dot(Theta)))).T.dot(Y) 
++ (np.log(1 - sigmoid(X.dot(Theta)))).T.dot(1 - Y)) / X.shape[0])
+    + (1 / (2 * X.shape[0])) * (Theta[1:,]**2).sum())
 
 # La operacion que hace el gradiente por dentro. Devueve un vector de valores
-def gradient(O, X, Y, l):
-    AuxO = np.hstack([np.zeros([1]), O[1:,]])
-    return (((X.T.dot(sigmoid(X.dot(O)) - np.ravel(Y))) / X.shape[0]) 
+def gradient(Theta, X, Y, l):
+    AuxO = np.hstack([np.zeros([1]), Theta[1:,]])
+    return (((X.T.dot(sigmoid(X.dot(Theta)) - np.ravel(Y))) / X.shape[0]) 
     + (l / X.shape[0]) * AuxO)
 
 # Para cada ejemplo (fila de Xs), haya los pesos theta por cada posible tipo de numero que pueda ser
 def oneVsAll(X, Y, numEtiquetas, reg):
     clase = 10
-    O = np.zeros([numEtiquetas, X.shape[1]])
+    Theta = np.zeros([numEtiquetas, X.shape[1]])
 
     for i in range(numEtiquetas):
         claseVector = (Y == clase)
 
-        result = opt.fmin_tnc(func = cost, x0 = O[i], fprime = gradient, args = (X, claseVector, reg))
-        O[i] = result[0]
+        result = opt.fmin_tnc(func = cost, x0 = Theta[i], fprime = gradient, args = (X, claseVector, reg))
+        Theta[i] = result[0]
 
         if clase == 10:
             clase = 1
         else:
             clase += 1
-    return O
+    return Theta
 
 # Determina el porcentaje de aciertos de la regresion logistica multicapa comparando los resultados estimados con los resultados reales
-def logisticSuccessPercentage(X, Y, O):
+def logisticSuccessPercentage(X, Y, Theta):
     numAciertos = 0
 
     for i in range(X.shape[0]):
-        results = sigmoid(X[i].dot(O.T))
+        results = sigmoid(X[i].dot(Theta.T))
         maxResult = np.argmax(results)
         if maxResult == 0:
             maxResult = 10
@@ -67,10 +67,10 @@ def neuronalSuccessPercentage(results, Y):
         if result == Y[i]: numAciertos += 1
     return (numAciertos / (results.shape[0])) * 100
 
-def propagacion(X1, O1, O2):
-    X2 = sigmoid(X1.dot(O1.T))
+def propagacion(X1, Theta1, Theta2):
+    X2 = sigmoid(X1.dot(Theta1.T))
     X2 = np.hstack([np.ones([X2.shape[0], 1]), X2])
-    return sigmoid(X2.dot(O2.T))
+    return sigmoid(X2.dot(Theta2.T))
 
 def main():
     valores = loadMat("ex3data1.mat")
@@ -86,17 +86,17 @@ def main():
 
     # Cuanto mas se aproxime a 0, mas se ajustara el polinomio (menor regularizacion)
     l = 0.1
-    O = oneVsAll(X, Y, numEtiquetas, l)
+    Theta = oneVsAll(X, Y, numEtiquetas, l)
 
-    success = logisticSuccessPercentage(X, Y, O)
-    print("Logistic regression success: " + str(success) + " %")
+    success = logisticSuccessPercentage(X, Y, Theta)
+    print("Precisión de la regresión logística: " + str(success) + " %")
 
     # Redes neuronales
     weights = loadMat('ex3weights.mat')
-    O1, O2 = weights['Theta1'], weights['Theta2']
+    Theta1, Theta2 = weights['Theta1'], weights['Theta2']
 
-    success = neuronalSuccessPercentage(propagacion(X, O1, O2), Y)
-    print("Neuronal network success: " + str(success) + " %")
+    success = neuronalSuccessPercentage(propagacion(X, Theta1, Theta2), Y)
+    print("Precisión de la red neuronal: " + str(success) + " %")
 
     graphics(X[:, 1:])
 
